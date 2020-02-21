67b8f2e0a065a690e5ad1068bbdf217200e02fbb
Kevin <aoenth@gmail.com>
2020-02-21 15:08:56 -0500

    Refactor, DetailsView class

--- ChineseZodiac/DetailsVC.swift
+++ ChineseZodiac/DetailsVC.swift
@@ -9,108 +9,64 @@
 import UIKit
 
 final class DetailsVC: UIViewController {
-    @IBOutlet weak var zodiacImg: UIImageView!
-    @IBOutlet weak var nameLbl: UILabel!
-    @IBOutlet weak var zodiacLbl: UILabel!
+  
+  
+  @IBOutlet weak var deleteButton: UIButton!
+  @IBOutlet weak var cancelButton: UIBarButtonItem!
+  
+  var person: Person?
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet weak var birthdayLbl: UILabel!
-    @IBOutlet weak var cBirthdayLbl: UILabel!
-    @IBOutlet weak var stemBranchLbl: UILabel!
-    @IBOutlet weak var adYearLbl: UILabel!
-
-    @IBOutlet weak var lunarMonthLbl: UILabel!
-    @IBOutlet weak var fixedElementLbl: UILabel!
-    @IBOutlet weak var seasonLbl: UILabel!
-    @IBOutlet weak var solarTermLbl: UILabel!
-    @IBOutlet weak var compatibilityLbl: UILabel!
-    
-    @IBOutlet weak var deleteButton: UIButton!
-    @IBOutlet weak var cancelButton: UIBarButtonItem!
-    
-    var person: Person?
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        
-        
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "MMM d, yyyy"
-        if let person = person {
-            
-            let birthdate = person.birthdate! as Date
-            
-            // MARK: Image, name and zodiac
-            nameLbl.text = person.name
-            zodiacLbl.text = birthdate.getZodiac()
-            zodiacImg.image = UIImage(named: birthdate.getZodiac())
-            
-            // MARK: Birthday
-            birthdayLbl.text =  dateFormatter.string(from: birthdate)
-            
-            // MARK: Chinese Birthday
-            cBirthdayLbl.text = birthdate.formatChineseCalendarDate()
-            
-            // MARK: Stem-Branch
-            stemBranchLbl.text = birthdate.getStemBranch()
-
-            // MARK: AD Year
-            if birthdate.getAhYear() > 1 {
-                adYearLbl.text = "\(birthdate.getAhYear()) AH"
-            } else {
-                adYearLbl.text = "\(birthdate.getAhYear()) BH"
-            }
-            // MARK: Lunar Month
-            lunarMonthLbl.text = birthdate.getLunarMonth()
-            // MARK: Season
-            seasonLbl.text = birthdate.getSeason()
-            // MARK: Solar Term
-            solarTermLbl.text = birthdate.getSolarTerm()
-            fixedElementLbl.text = birthdate.getFixedElement()
-            var compatibility = ""
-            let zodiacRank = Int(person.zodiac)
-            for i in 1...12 {
-                let bestZodiacForSelf = Helper.match(person1: zodiacRank, person2: i)
-                if bestZodiacForSelf == 6 {
-                    compatibility += "\(Helper.getZodiac(fromIndex: i)), "
-                }
-            }
-            let last2Chars = compatibility.index(compatibility.endIndex, offsetBy: -2)
-            compatibility.removeSubrange(last2Chars..<compatibility.endIndex)
-            compatibilityLbl.text = compatibility
-        }
+    updateInformation()
+  }
+  
+  fileprivate func updateInformation() {
+    guard let person = person else {
+      return
     }
-
-
-    @IBAction func cancelButtonPressed(_ sender: Any) {
-        navigationController?.popToRootViewController(animated: true)
+    guard let view = view as? DetailsView else {
+      return
+    }
+    view.updateInformation(forPerson: person)
+  }
+  
+  @IBAction func cancelButtonPressed(_ sender: Any) {
+    navigationController?.popToRootViewController(animated: true)
+  }
+  
+  override func setEditing(_ editing: Bool, animated: Bool) {
+    super.setEditing(editing, animated: animated)
+    if(self.isEditing) {
+      self.editButtonItem.title = "Done"
+      deleteButton.isHidden = false
+    } else {
+      self.editButtonItem.title = "Edit"
+      deleteButton.isHidden = true
+    }
+  }
+  
+  
+  @IBAction func EditButtonPressed(_ sender: Any) {
+    performSegue(withIdentifier: "EditPerson", sender: self.person)
+  }
+  
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    guard segue.identifier == "EditPerson" else {
+      return
+    }
+    guard let destination = segue.destination as? BirthdaySelectionView else {
+      return
     }
     
-    override func setEditing(_ editing: Bool, animated: Bool) {
-        super.setEditing(editing, animated: animated)
-        if(self.isEditing) {
-            self.editButtonItem.title = "Done"
-            deleteButton.isHidden = false
-        } else {
-            self.editButtonItem.title = "Edit"
-            deleteButton.isHidden = true
-        }
+    guard let person = sender as? Person else {
+      return
     }
     
+    destination.personToEdit = person
     
-    @IBAction func EditButtonPressed(_ sender: Any) {
-        performSegue(withIdentifier: "EditPerson", sender: self.person)
-    }
-    
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "EditPerson" {
-            if let destination = segue.destination as? BirthdaySelectionView {
-                if let person = sender as? Person {
-                    destination.personToEdit = person
-                }
-            }
-        }
-    }
-
-
+  }
+  
+  
 }
